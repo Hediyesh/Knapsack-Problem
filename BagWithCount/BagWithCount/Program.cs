f56803eafffa72c05a0d3cfa127aee02d7b07d45
unknown <hediye.shariaty@gmail.com>
2025-04-12 18:25:24 +0330

    Adding the project

---
+++ BagWithCount/BagWithCount/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace BagWithCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("please enter number of products:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("please enter weight of bag:");
+                int W = Convert.ToInt32(Console.ReadLine());
+                int[] v = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("please enter worth of poduct {0}:",i+1);
+                    v[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                int[] w = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("please enter weight of poduct {0}:",i+1);
+                    w[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                int[] c = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("please enter count of poduct {0}:",i+1);
+                    c[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                var v1 = MaxVWithoutCount(n, v, w, W);
+                Console.WriteLine("max value without count: " + v1);
+                var v2 = MaxVWithCount(n, v, w, c, W);
+                Console.WriteLine("max value with count: " + v2);
+                Console.WriteLine("Do you want to exit? enter yes or no:");
+                str = Console.ReadLine();
+            } while (str == "no");
+        }
+        public static int MaxVWithCount(int n, int[] v, int[] w, int[] c, int W)
+        {
+            int[,] V = new int[n + 1, W + 1];
+            for (int i = 0; i <= W; i++)
+            {
+                V[0, i] = 0;
+            }
+            for (int i = 0; i <= n; i++)
+            {
+                V[i, 0] = 0;
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j <= W; j++)
+                {
+                    int count = 1;
+                    for (int t = 1; t <= c[i - 1]; t++)
+                    {
+                        if (j - t * w[i - 1] < 0)
+                        {
+                            count = t - 1;
+                            break;
+                        }
+                        else
+                        {
+                            count = t;
+                        }
+                    }
+                    if (j >= w[i - 1] && V[i - 1, j - count * w[i - 1]] + count * v[i - 1] > V[i - 1, j])
+                    {
+                        V[i, j] = V[i - 1, j - count * w[i - 1]] + count * v[i - 1];
+                    }
+                    else
+                    {
+                        V[i, j] = V[i - 1, j];
+                    }
+                }
+            }
+            return V[n, W];
+        }
+        public static int MaxVWithoutCount(int n, int[] v, int[] w, int W)
+        {
+            int[,] V = new int[n + 1, W + 1];
+            for (int i = 0; i <= W; i++)
+            {
+                V[0, i] = 0;
+            }
+            for (int i = 0; i <= n; i++)
+            {
+                V[i, 0] = 0;
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j <= W; j++)
+                {
+                    if (j >= w[i - 1] && V[i - 1, j - w[i - 1]] + v[i - 1] > V[i - 1, j])
+                    {
+                        V[i, j] = V[i - 1, j - w[i - 1]] + v[i - 1];
+
+                    }
+                    else
+                    {
+                        V[i, j] = V[i - 1, j];
+                    }
+                }
+            }
+            return V[n, W];
+        }
+    }
+}
---
+++ BagWithDoubleWeight/BagWithDoubleWeight/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BagWithDoubleWeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;double W = 17.8;
+            //Worth
+            double[] v = { 10, 40, 30, 50 };
+            //Weight
+            double[] w = { 5.2, 4.1, 6.3, 3.7 };
+            Max(n, v, w, W);
+        }
+        public static void Max(int n, double[] v, double[] w, double W)
+        {
+            string str = "";
+            double sum = 0;
+            double[] nesbat = new double[v.Length];
+            double[] pickedItems = new double[v.Length];
+            double wsum = 0;
+            int index = 0;
+            while (index < v.Length)
+            {
+                nesbat[index] = v[index] / w[index];
+                pickedItems[index] = 0;
+                index++;
+            }
+            int c = 1;
+            while (wsum < W && c<=n)
+            {
+                int best = 0;
+                index = 0;
+                while (index < v.Length)
+                {
+                    if (pickedItems[index] == 0 && nesbat[best] < nesbat[index])
+                    {
+                        best = index;
+                    }
+                    index++;
+                }
+                if (wsum + w[best] <= W)
+                {
+                    str = str + "product(" + best.ToString()+ ") Has been picked completely"+"\n";
+                    sum += v[best];
+                    wsum += w[best];
+                    pickedItems[best] = 1;
+                    nesbat[best] = -1;
+                }
+                else
+                {
+                    pickedItems[best] = v[best] * (W - wsum) / w[best];
+                    str = str + "product(" + best.ToString() +") Has been picked with %" + (pickedItems[best] / v[best] * 100).ToString()+"\n";
+                    sum = sum + (v[best] * (W - wsum) / w[best]);
+                    wsum = W;
+                }
+                c++;
+            }
+            str += "Whole value = " + sum.ToString()  ;
+            Console.WriteLine(str);
+        }
+    }
+}
